90b40239a798f7e94e3665d6c6a86cc0e52e9ab2
Dmitry Vavshko <hivens.mailbox@gmail.com>
2018-06-16 12:34:37 +0200

    fix

--- js/swap-css.js
+++ js/swap-css.js
@@ -12,27 +12,9 @@ $(document).ready(function() {
     $("link[media=print]").attr({href : "css/print-no-color.css"});
     });
 
-    // $('img#1.print-color').on('click', function(){
-    
-    //     $(this).addClass('active');
-    // });
-
-    // $('img#2.print-color-pdf').on('click', function(){
-    
-    //     $(this).addClass('active');
-    // });
-
-    // $('img#3.print-color-no').on('click', function(){
-    
-    //     $(this).addClass('active');
-    // });
-
     $("img[id]").click(function(){
         $(".active").removeClass("active");
         $(this).addClass("active")
    
     });
-
     });
-
-   
