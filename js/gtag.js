19f826fd72ad35d95914a6cd70b70bb9d031a055
Dmitry Vavshko <hivens.mailbox@gmail.com>
2018-07-05 12:58:10 +0300

    fix

--- js/gtag.js
+++ js/gtag.js
@@ -1,5 +1,5 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-72237423-5"></script>
+
 <script>
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
