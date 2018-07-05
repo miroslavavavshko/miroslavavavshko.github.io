5437d0de4ee6e307a760e7d1bcc427b0ebbca752
Dmitry Vavshko <hivens.mailbox@gmail.com>
2018-07-05 13:16:02 +0300

    fix

--- js/gtag.js
+++
@@ -1,9 +0,0 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-72237423-5"></script>
-
-<script>
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'UA-72237423-5');
-</script>
